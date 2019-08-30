74a55460b47aeb087b016afd098126b4999dec66
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-08-30 00:28:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,8 +31,8 @@ end
 def using_delete(array, element)
   return array.delete(element)
 end
-def using_delete_at(array, )
-  return array.delete()
+def using_delete_at(array, index)
+  return array.delete(index)
 end
   
   
